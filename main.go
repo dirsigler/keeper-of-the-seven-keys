cabdb1a028b1c2a4b2a9b401c787cc6774e70490
Dennis Irsigler <dennis.irsigler@metro-markets.de>
2025-04-09 08:37:30 +0200

    Use main function

--- main.go
+++ main.go
@@ -3,18 +3,31 @@ package main
 import (
 	"encoding/json"
 	"fmt"
+	"io"
 	"log"
 	"net/http"
-
-	"github.com/GoogleCloudPlatform/functions-framework-go/functions"
+	"os"
 )
 
 // EventarcPayload represents the structure of the Eventarc JSON payload.
 // We'll use an interface{} to handle the dynamic nature of the payload.
 type EventarcPayload map[string]interface{}
 
-func init() {
-	functions.HTTP("eventHandler", eventHandler)
+func main() {
+	// Determine the port for HTTP service.
+	port := os.Getenv("PORT")
+	if port == "" {
+		port = "8080"
+		log.Printf("Defaulting to port %s", port)
+	}
+
+	http.HandleFunc("/", eventHandler)
+
+	log.Printf("Listening on port %s", port)
+
+	if err := http.ListenAndServe(":"+port, nil); err != nil {
+		log.Fatalf("Failed to start server: %v", err)
+	}
 }
 
 func eventHandler(w http.ResponseWriter, r *http.Request) {
@@ -25,18 +38,24 @@ func eventHandler(w http.ResponseWriter, r *http.Request) {
 		return
 	}
 
-	// Decode the JSON payload
-	var payload EventarcPayload
-
-	decoder := json.NewDecoder(r.Body)
-	if err := decoder.Decode(&payload); err != nil {
-		log.Printf("Failed to decode JSON payload: %v", err)
-		http.Error(w, "Failed to decode JSON payload", http.StatusBadRequest)
+	// Read the request body
+	body, err := io.ReadAll(r.Body)
+	if err != nil {
+		log.Printf("Failed to read request body: %v", err)
+		http.Error(w, "Failed to read request body", http.StatusBadRequest)
 
 		return
 	}
-
 	defer r.Body.Close()
+
+	// Unmarshal the JSON payload
+	var payload EventarcPayload
+	if err := json.Unmarshal(body, &payload); err != nil {
+		log.Printf("Failed to unmarshal JSON payload: %v", err)
+		http.Error(w, "Failed to unmarshal JSON payload", http.StatusBadRequest)
+
+		return
+	}
 
 	// Pretty print the payload
 	prettyJSON, err := json.MarshalIndent(payload, "", "  ")
